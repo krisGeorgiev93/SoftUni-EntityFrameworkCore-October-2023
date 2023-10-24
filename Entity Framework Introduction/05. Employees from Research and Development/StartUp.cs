2f6367adeed13dfd0f78d6a6c432d7ff67f82cf1
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-24 22:19:26 +0100

    add tasks

---
+++ Entity Framework Introduction/05. Employees from Research and Development/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+    }      
+
+    //problem 05
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .OrderBy(e => e.Salary)
+            .ThenByDescending(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            stringBuilder.AppendLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary:f2}");
+        }
+
+        return stringBuilder.ToString().TrimEnd();
+    }
+
+
+
+}
