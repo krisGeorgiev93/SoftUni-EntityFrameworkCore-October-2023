32a4e676eea1660b000d72a8bafd84640544e8bd
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-24 22:19:16 +0100

    add tasks

---
+++ Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+    }
+    
+    //problem 04
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Salary > 50000)
+            .OrderBy(e => e.FirstName)
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            stringBuilder.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+        }
+
+        return stringBuilder.ToString().TrimEnd();
+    }
+
+
+
+
+}
