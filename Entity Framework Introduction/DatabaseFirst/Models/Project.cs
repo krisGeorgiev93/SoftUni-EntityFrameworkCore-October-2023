35d836790991bac2439f6495bc15d13ba577ca0b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-24 22:18:46 +0100

    add tasks

---
+++ Entity Framework Introduction/DatabaseFirst/Models/Project.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUni.Models
+{
+    public partial class Project
+    {
+        public Project()
+        {
+            this.EmployeesProjects = new HashSet<EmployeeProject>(); //change with EP to make the relation
+        }
+
+        public int ProjectId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public virtual ICollection<EmployeeProject> EmployeesProjects { get; set; }
+    }
+}
---
+++ Entity Framework Introduction/DatabaseFirst/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();     
+    }
+
+}
