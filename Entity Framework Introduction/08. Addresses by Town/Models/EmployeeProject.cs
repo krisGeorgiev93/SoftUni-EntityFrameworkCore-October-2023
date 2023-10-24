4427784cfd91ee9cad5a34936dd113fbb1aed247
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-24 22:20:01 +0100

    ad tasks

---
+++ Entity Framework Introduction/08. Addresses by Town/Models/EmployeeProject.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftUni.Models;
+//make new class with the primary keys
+
+public class EmployeeProject
+{
+    public int EmployeeId { get; set; }
+
+    public virtual Employee Employee { get; set; }
+
+    public int ProjectId { get; set; }
+
+    public virtual Project Project { get; set; }
+}
---
+++ Entity Framework Introduction/08. Addresses by Town/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SoftUni.Data;
+using SoftUni.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext context = new SoftUniContext();
+    }
+
+    //problem 08
+    public static string GetAddressesByTown(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var addresses = context.Addresses
+            .OrderByDescending(e => e.Employees.Count())
+        .ThenBy(a => a.Town!.Name)
+        .ThenBy(a => a.AddressText)
+        .Take(10)
+        .ToArray();
+
+        foreach (var a in addresses)
+        {
+            sb.AppendLine($"{a.AddressText}, {a.Town!.Name} - {a.Employees.Count()} employees");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
