7d56d45856045245ca9cc1fa95fa3faacf33a5ac
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-27 23:48:51 +0100

    add tasks

---
+++ Entity Framework Introduction/13. Find Employees by First Name Starting With Sa/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();         
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(x => x.FirstName.StartsWith("Sa"))
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x => x.FirstName)
+                .ThenBy(x => x.LastName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle} - (${employee.Salary:F2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
