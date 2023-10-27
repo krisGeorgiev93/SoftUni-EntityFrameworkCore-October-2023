2da2601001fce9f53edfe4aec467029cba4e3e15
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-27 23:47:34 +0100

    add tasks

---
+++ Entity Framework Introduction/09. Employee 147/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            string result = GetEmployee147(context);
+            Console.WriteLine(result);
+        }
+
+     
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employee147 = context.Employees.FirstOrDefault(e => e.EmployeeId == 147);
+
+            var projects = context.Projects
+                .Where(p => p.EmployeesProjects.Any(p => p.EmployeeId == employee147.EmployeeId))
+                .Select(p => new { ProjectName = p.Name })
+                .OrderBy(ep => ep.ProjectName);
+
+            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+
+            foreach (var project in projects)
+            {
+                sb.AppendLine(project.ProjectName);
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
+
