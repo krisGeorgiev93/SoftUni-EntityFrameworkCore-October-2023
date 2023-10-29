5b4830072417cb06c5c049a0031a40cd08ec84b0
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-10-29 20:07:36 +0000

    upload exercises

---
+++ Entity Relations/StudentSystem/Data/Models/Course.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+            Homeworks = new HashSet<Homework>();
+            Resources = new HashSet<Resource>();
+            StudentsCourses = new HashSet<StudentCourse>();
+        }
+
+        public int CourseId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ICollection<Homework> Homeworks { get; set; }
+
+        public ICollection<Resource> Resources { get; set; }
+
+        public ICollection<StudentCourse> StudentsCourses { get; set; }
+
+    }
+
+}
