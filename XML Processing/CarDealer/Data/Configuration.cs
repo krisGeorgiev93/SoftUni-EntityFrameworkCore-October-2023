b7dccf9f969fd3d4d3dcdac21bb8694f5c0bbae1
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-11-19 22:26:37 +0000

    upload data

--- XML Processing/CarDealer/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CarDealer.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-2ASBARL\SQLEXPRESS;Database=CarDealer;Integrated Security=True;Encrypt=False";
-    }
-}
