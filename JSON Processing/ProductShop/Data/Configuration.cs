34ef5a26f9a823e20bf6d1beeb80c02725363e68
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-11-19 22:27:08 +0000

    upload data

--- JSON Processing/ProductShop/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductShop.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString =
-            @"Server=DESKTOP-2ASBARL\SQLEXPRESS;Database=ProductShop;Integrated Security=True;Encrypt=False;";
-    }
-}
