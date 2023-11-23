aa534252f25c040e81c27b975dadeb17dc0c7be8
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-11-23 22:48:23 +0000

    remove config

--- Exam Preparation/Boardgames/Boardgames/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Boardgames.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-6VUQ1VB\SQLEXPRESS;Database=Boardgames;Integrated Security=True;Encrypt=False";
-    }
-}
