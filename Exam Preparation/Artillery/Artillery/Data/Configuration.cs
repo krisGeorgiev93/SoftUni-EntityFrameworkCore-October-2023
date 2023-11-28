f9ca35c60b0b00c87ca26c68760c0c1b8f950d7a
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-11-28 21:32:02 +0000

    update

--- Exam Preparation/Artillery/Artillery/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Artillery.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-6VUQ1VB\SQLEXPRESS;Database=Artillery;Integrated Security=True;Encrypt=False";
-    }
-}
