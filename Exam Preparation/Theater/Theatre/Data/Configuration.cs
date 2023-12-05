6d52fb6e59b9f1e82e3b5803e68988614ce42909
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-12-05 23:09:01 +0000

    delete config

--- Exam Preparation/Theater/Theatre/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Theatre.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-6VUQ1VB\SQLEXPRESS;Database=Theatre;Integrated Security=True;Encrypt=False";
-    }
-}
