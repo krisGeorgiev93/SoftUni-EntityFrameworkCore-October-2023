bce9370021c1da8e1548512e76b85a483e05ae0e
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-12-11 21:25:41 +0000

    update

--- Exam Preparation/TeisterMask/TeisterMask/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TeisterMask.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-6VUQ1VB\SQLEXPRESS;Database=TeisterMaskExam;Integrated Security=True;Encrypt=False";
-    }
-}
