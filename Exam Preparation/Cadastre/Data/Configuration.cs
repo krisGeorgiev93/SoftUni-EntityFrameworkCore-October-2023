562281c140d358b85a9ca3b28c34f9e6a7e4abfa
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-12-18 22:59:31 +0000

    update

--- Exam Preparation/Cadastre/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cadastre.Data
-{
-    public class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-6VUQ1VB\SQLEXPRESS;Database=Cadastre;Integrated Security=True;Encrypt=False";
-
-    }
-}
